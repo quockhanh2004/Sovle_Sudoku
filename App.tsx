d767b37a47619aaa999d43d919226dba3bbda810
Phạm Ngọc Quốc Khánh <phamngocquockhanh2004@gmail.com>
2024-10-02 00:37:20 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import {StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import Demo from './src/Demo';
+
+const App = () => {
+  return <Demo />;
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
